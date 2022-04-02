ef28a33881e0fb0c485b6ea5b77fe37fa1d6e544
alex.sy.tang <alex.sy.tang@gmail.com>
2022-04-01 22:57:32 -1000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,150 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+
+const app = express()
+
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine','ejs')
+app.use(express.static("public"))
+
+main().catch(err=>console.log(err));
+
+async function main(){
+
+  await mongoose.connect('mongodb://localhost:27017/userDB');
+  const userSchema = new mongoose.Schema({
+    usrName:{
+      type:String,
+      required:true,
+    },
+    password:{
+      type:String,
+      required:true
+    }
+  })
+  let secret = process.env.SECRET_KEY
+  userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+  const User = mongoose.model('User',userSchema);
+
+  app.listen('3000',()=>{
+    console.log('Starting on port 3000')
+  })
+
+  app.get('/',(req,res)=>{
+    res.render('home')
+  })
+
+  app.get('/login',(req,res)=>{
+    res.render('login')
+  })
+
+  app.get('/register',(req,res)=>{
+    res.render('register')
+  })
+
+  app.get('/secrets',(req,res)=>{
+    res.render('secrets')
+  })
+
+app.post('/register',(req,res)=>{
+  const password = req.body.password
+  const username = req.body.username
+  const newUser = new User({
+    usrName:username,
+    password:password
+  })
+  newUser.save(err=>{
+    if(err){
+      console.log(err)
+    }else{
+      res.render('secrets')
+    }
+  })
+})
+
+
+app.post('/login',(req,res)=>{
+  const username = req.body.username
+  const password = req.body.password
+  User.findOne({usrName:username},(err,userFound)=>{
+    if(err){
+      console.log(err)
+    }else{
+      if(!userFound){
+        console.log("This account hasn't been created ")
+        res.redirect('/login')
+      }else{
+          if(userFound.password === password){
+            res.redirect('/secrets');
+          }
+
+      }
+    }
+  })
+})
+}
+
+//   app.get('/secrets',(req,res)=>{
+//     res.render('secrets')
+//   })
+//
+// app.post('/register',(req,res)=>{
+//   const password = req.body.password
+//   const username = req.body.username
+//   const newUser = new User({
+//     usrName:username,
+//     password:password
+//   })
+//   newUser.save(err=>{
+//     if(err){
+//       console.log(err)
+//     }else{
+//       res.render('secrets')
+//     }
+//   })
+// })
+//
+//
+// app.post('login',(req,res)=>{
+//   const username = req.body.username
+//   const password = req.body.password
+//   User.findOne({usrName:username},(err,userFound)=>{
+//     if(err){
+//       console.log(err)
+//     }else{
+//       if(!userFound){
+//         console.log("This account hasn't been created ")
+//         res.redirect('/login')
+//       }else{
+//         if(userFound.usrName === username){
+//           if(userFound.password === password){
+//             res.redirect('/secrets');
+//           }
+//         }
+//       }
+//     }
+//   })
+// })
+
+// app.listen('3000',()=>{
+//   console.log('Starting on port 3000')
+// })
+//
+// app.get('/',(req,res)=>{
+//   res.render('home')
+// })
+//
+// app.get('/login',(req,res)=>{
+//   res.render('login')
+// })
+//
+// app.get('/register',(req,res)=>{
+//   res.render('register')
+// })
